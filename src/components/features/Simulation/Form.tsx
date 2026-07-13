8d43f97c9fd92d217e35bbe85fa3c7bbdbd41bca
moacirrochadev <empreendedormoacirneto@gmail.com>
2026-07-13 17:01:22 -0300

    Implementando a navegação entre as etapas do formulário

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,17 +1,40 @@
 import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 import { simulationFormSteps } from '../../../data/simulation'
+import { useState } from 'react'
 
 export function SimulationForm() {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex] // Example: Get the first step for demonstration
 
-  const currentStep = simulationFormSteps[1] // Example: Get the first step for demonstration
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) {
+      return
+    }
+    setCurrentStepIndex((prev) => prev - 1)
+  }
 
   return (
     <>
-      <StepProgress currentStep={1} totalSteps={6} />
+      <StepProgress
+        currentStep={currentStepIndex + 1}
+        totalSteps={totalSteps}
+      />
       <FormStep
         key={currentStep.id}
         {...currentStep}
+        onBack={handlePreviousStep}
+        onNext={handleNextStep}
+        hideBackButton={currentStepIndex === 0}
       />
     </>
   )
--- src/components/features/Simulation/FormStep.tsx
+++ src/components/features/Simulation/FormStep.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeft, ArrowRight, type LucideIcon } from 'lucide-react'
 import { Input, type InputProps } from '../../shared/Input'
 import { Button } from '../../shared/Button'
+import { useState, type SyntheticEvent } from 'react'
 
 export interface FormStepProps {
   id: string
@@ -14,13 +15,34 @@ export interface FormStepProps {
   }
 }
 
+interface ActionsButtonProps {
+  onBack?: () => void
+  onNext?: () => void
+  hideBackButton?: boolean
+}
+
 export function FormStep({
   icon: Icon,
   title,
   question,
   inputProps,
   submitButtonProps,
-}: FormStepProps) {
+  hideBackButton,
+  onBack,
+  onNext,
+}: FormStepProps & ActionsButtonProps) {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(!inputValue){
+      return
+    }
+
+    onNext?.()
+  }
+
   return (
     <div className="bg-card rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] sm:p-8">
       <div className="bg-primary mb-4 flex h-15 w-15 items-center justify-center rounded-xl">
@@ -32,22 +54,30 @@ export function FormStep({
       <h3 className="text-foreground mb-6 text-xl leading-snug font-semibold sm:text-2xl">
         {question}
       </h3>
-      <form className="flex flex-col gap-4">
-        <Input {...inputProps} />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <Input
+          {...inputProps}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
         <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
-          <Button
-            type="button"
-            // onclick={onBack}
-            variant="ghost"
-            icon={ArrowLeft}
-            className="order-2 flex-1 justify-center rounded-xl py-3 sm-order-1"
-          >
-            Voltar
-          </Button>
+          {!hideBackButton && (
+            <Button
+              type="button"
+              onClick={onBack}
+              variant="ghost"
+              icon={ArrowLeft}
+              className="order-2 flex-1 justify-center rounded-xl py-3 sm-order-1"
+            >
+              Voltar
+            </Button>
+          )}
+
           <Button
             type="submit"
             variant="primary"
             icon={!submitButtonProps ? ArrowRight : undefined}
+            disabled={!inputValue}
             className="order-1 flex-1 sm:order-2"
           >
             {submitButtonProps?.label ?? 'Próximo'}
